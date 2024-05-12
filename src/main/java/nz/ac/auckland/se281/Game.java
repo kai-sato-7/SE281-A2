630237734eff6580ec9e13e27463e94414e26b86
Kai Sato <kai.sato321@gmail.com>
2024-05-12 23:12:51 +1200

    Added round number message in newGame

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,8 +6,11 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  private int round = 0;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    MessageCli.START_ROUND.printMessage(String.valueOf(++round));
   }
 
   public void play() {
