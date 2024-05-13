b4be1615e91e09bc569284a4408ea86615f97220
Kai Sato <kai.sato321@gmail.com>
2024-05-13 21:45:12 +1200

    Added medium strategy class logic

--- src/main/java/nz/ac/auckland/se281/Medium.java
+++ src/main/java/nz/ac/auckland/se281/Medium.java
@@ -2,8 +2,13 @@ package nz.ac.auckland.se281;
 
 public class Medium implements Diff {
 
+    private int round = 1;
+
     @Override
     public Strategy getStrategy() {
-        return new RandomStrategy();
+        if (++round < 4) {
+            return new RandomStrategy();
+        }
+        return new TopStrategy();
     }
 }
