b0542c542f3744d5c0447867b7f5448036da1168
Kai Sato <kai.sato321@gmail.com>
2024-05-13 14:24:13 +1200

    Moved start round message to play function

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -7,13 +7,19 @@ import nz.ac.auckland.se281.Main.Difficulty;
 public class Game {
 
   private int round = 0;
+  private Difficulty difficulty;
+  private Choice choice;
+  private String[] options;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
+    this.difficulty = difficulty;
+    this.choice = choice;
+    this.options = options;
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
-    MessageCli.START_ROUND.printMessage(String.valueOf(++round));
   }
 
   public void play() {
+    MessageCli.START_ROUND.printMessage(String.valueOf(++round));
   }
 
   public void endGame() {
